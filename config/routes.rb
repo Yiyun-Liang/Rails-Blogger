58d97d3c65694d0feb039150882b4f51d9f7297c
isa <isaliangyiyun@hotmail.com>
2017-07-02 20:18:12 -0400

    rails generate controller articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # routers then understands the following:
+  # http://localhost:3000/articles/
+  # http://localhost:3000/articles/new
+  resources :articles
 end
